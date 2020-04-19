9056f7e0ca3a8380640198b161bb0ebdb1ec24a3
Czeffik <trzewik93@gmail.com>
2020-04-19 03:14:33 +0200

    Add integration tests for all InformationController endpoints

--- src/test/integration/com/trzewik/information/consumer/interfaces/rest/information/RestInterfacesTestConfig.groovy
+++ src/test/integration/com/trzewik/information/consumer/interfaces/rest/information/RestInterfacesTestConfig.groovy
@@ -3,10 +3,13 @@ package com.trzewik.information.consumer.interfaces.rest.information
 import com.trzewik.information.consumer.domain.information.InformationService
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import spock.mock.DetachedMockFactory
 
+@Profile(RestInterfacesTestConfig.PROFILE)
 @TestConfiguration
 class RestInterfacesTestConfig {
+    final static String PROFILE = 'rest-interfaces-test-config'
     DetachedMockFactory factory = new DetachedMockFactory()
 
     @Bean
